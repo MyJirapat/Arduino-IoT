b0620c02449e77e61ca49c481394d5cbd020e58a
Jirapat Suwanjunee <suwanjunee987@gmail.com>
2022-02-25 09:56:41 +0700

    Node MPU

---
+++ 5 Led/5Led.ino
@@ -0,0 +1,164 @@
+/* 8 LEDs blinking at random intervals without delay
+ Using the millis method you don't have to wait for an operation to happen before executing the next bit of code.  
+ If you don't use this method you can't overlap state changes.
+ */
+
+// constants won't change. Used here to 
+// set pin numbers:
+
+const int ledPin4 = 1;
+
+const int ledPin6 = 4;
+const int ledPin7 = 3;
+const int ledPin8 = 5;
+const int buzzPin = 2;
+
+// Variables will change:
+int ledState = LOW;
+int ledState2 = LOW;
+int ledState3 = LOW;
+int ledState4 = LOW;
+int ledState5 = LOW;
+int ledState6 = LOW;
+int ledState7 = LOW;
+int ledState8 = LOW;
+int buzzState = LOW;// ledState used to set the LED
+long previousMillis = 0;
+long previousMillis2 = 0;
+long previousMillis3 = 0;
+long previousMillis4 = 0;
+long previousMillis5 = 0;
+long previousMillis6 = 0;
+long previousMillis7 = 0;
+long previousMillis8 = 0;
+long previousBuzzMillis = 0;// will store last time LED was updated
+
+// the follow variables is a long because the time, measured in miliseconds,
+// will quickly become a bigger number than can be stored in an int.
+long interval = 10000;
+long interval2 = 9000;
+long interval3 = 8000;
+long interval4 = 7000;
+long interval5 = 6000;
+long interval6 = 5000;
+long interval7 = 4000;
+long interval8 = 3000;
+long intervalBuzz = 150;// interval at which to blink (milliseconds)
+
+void setup() {
+  
+  pinMode(ledPin4, OUTPUT); 
+  //pinMode(ledPin5, OUTPUT); 
+  //pinMode(ledPin6, OUTPUT); 
+  pinMode(ledPin7, OUTPUT); 
+  pinMode(ledPin8, OUTPUT); 
+  pinMode(buzzPin, OUTPUT);
+
+}
+
+void loop()
+{
+  
+  unsigned long currentMillis = millis();
+
+  if(currentMillis - previousMillis > random(200, interval)) {
+    // save the last time you blinked the LED 
+    previousMillis = currentMillis;   
+
+    // if the LED is off turn it on and vice-versa:
+    if (ledState == LOW)
+      ledState = HIGH;
+    else
+      ledState = LOW;
+
+    // set the LED with the ledState of the variable:
+   // digitalWrite(ledPin, ledState);
+  }
+
+  unsigned long currentMillis4 = millis();
+  if(currentMillis4 - previousMillis4 > random(200, interval4)) {
+    // save the last time you blinked the LED 
+    previousMillis4 = currentMillis4;   
+
+    // if the LED is off turn it on and vice-versa:
+    if (ledState4 == LOW)
+      ledState4 = HIGH;
+    else
+      ledState4 = LOW;
+
+    // set the LED with the ledState of the variable:
+    digitalWrite(ledPin4, ledState4);
+  }
+
+
+
+  unsigned long currentMillis5 = millis();
+  if(currentMillis5 - previousMillis5 > random(200, interval5)) {
+    // save the last time you blinked the LED 
+    previousMillis5 = currentMillis5;   
+
+    // if the LED is off turn it on and vice-versa:
+    if (ledState5 == LOW)
+      ledState5 = HIGH;
+    else
+      ledState5 = LOW;
+
+    // set the LED with the ledState of the variable:
+    //digitalWrite(ledPin5, ledState5);
+  }
+
+
+
+  unsigned long currentMillis6 = millis();
+  if(currentMillis6 - previousMillis6 > random(200, interval6)) {
+    // save the last time you blinked the LED 
+    previousMillis6 = currentMillis6;   
+
+    // if the LED is off turn it on and vice-versa:
+    if (ledState6 == LOW)
+      ledState6 = HIGH;
+    else
+      ledState6 = LOW;
+
+    // set the LED with the ledState of the variable:
+    digitalWrite(ledPin6, ledState6);
+  }
+
+
+
+  unsigned long currentMillis7 = millis();
+  if(currentMillis7 - previousMillis7 > random(200, interval7)) {
+    // save the last time you blinked the LED 
+    previousMillis7 = currentMillis7;   
+
+    // if the LED is off turn it on and vice-versa:
+    if (ledState7 == LOW)
+      ledState7 = HIGH;
+    else
+      ledState7 = LOW;
+
+    // set the LED with the ledState of the variable:
+    digitalWrite(ledPin7, ledState7);
+  }
+
+
+  unsigned long currentBuzzMillis = millis(); 
+  if(currentBuzzMillis - previousBuzzMillis > intervalBuzz) {
+    // save the last time you blinked the LED 
+    previousBuzzMillis = currentBuzzMillis;   
+
+    // if the LED is off turn it on and vice-versa:
+    if (buzzState == LOW) {
+      buzzState = HIGH;
+      intervalBuzz = 10000;
+    }
+    else {
+      buzzState = LOW;
+      intervalBuzz = 150;
+    }
+
+    // set the LED with the ledState of the variable:
+    digitalWrite(buzzPin, buzzState);
+  }
+
+}
---
+++ sketch_feb24a/sketch_feb24a.ino
@@ -0,0 +1,369 @@
+/*#include<Wire.h>
+#define BLYNK_PRINT Serial
+#include <Blynk.h>
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+//char auth[] = "TMPLukiBYK6W";       // You should get Auth Token in the Blynk App.
+//char ssid[] = "myjirapat_2.4G";                       // Your WiFi credentials.
+//char pass[] = "0853588010";
+
+const int MPU_addr=0x68;
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+ 
+int minVal=265;
+int maxVal=402;
+ 
+double x;
+double y;
+double z;
+ 
+void setup(){
+Wire.begin();
+Wire.beginTransmission(MPU_addr);
+Wire.write(0x6B);
+Wire.write(0);
+Wire.endTransmission(true);
+Serial.begin(9600);
+//Blynk.begin(auth, ssid, pass);
+ }
+void loop(){
+  
+Blynk.run();
+Wire.beginTransmission(MPU_addr);
+Wire.write(0x3B);
+Wire.endTransmission(false);
+Wire.requestFrom(MPU_addr,14,true);
+AcX=Wire.read()<<8|Wire.read();
+AcY=Wire.read()<<8|Wire.read();
+AcZ=Wire.read()<<8|Wire.read();
+int xAng = map(AcX,minVal,maxVal,-90,90);
+int yAng = map(AcY,minVal,maxVal,-90,90);
+int zAng = map(AcZ,minVal,maxVal,-90,90);
+ 
+x= RAD_TO_DEG * (atan2(-yAng, -zAng)+PI);
+y= RAD_TO_DEG * (atan2(-xAng, -zAng)+PI);
+z= RAD_TO_DEG * (atan2(-yAng, -xAng)+PI);
+ 
+Serial.print("AngleX= ");
+Serial.println(x);
+ 
+Serial.print("AngleY= ");
+Serial.println(y);
+ 
+Serial.print("AngleZ= ");
+Serial.println(z);
+Serial.println("-----------------------------------------");
+ 
+Blynk.virtualWrite(V1, x);
+Blynk.virtualWrite(V2, y);
+Blynk.virtualWrite(V3, z);
+ 
+delay(1000);
+}*/
+
+
+
+
+/*#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+#include "Wire.h"
+MPU6050 mpu;
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+int valx , valy , valz;
+char rd;
+int prevVal;
+int ledR = 16 ;
+int ledG = 14 ;
+int ledY = 12 ;
+int pin11 = 5 , pin10 = 4 ;
+int val1 , val2 ;
+int valgy1 = 0 , valgy2 = 0;
+void setup()
+{
+  pinMode(ledR, OUTPUT) ;
+  pinMode(ledG, OUTPUT) ;
+  pinMode(ledY, OUTPUT) ;
+  Wire.begin();
+  Serial.begin(38400);
+  Serial.println("Initialize MPU");
+  mpu.initialize();
+  Serial.println(mpu.testConnection() ? "Connected" : "Connection failed");
+}
+void loop()
+{
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  valx = map(ax, -17000, 17000, 0, 179);
+  valy = map(ay, -17000, 17000, 0, 179);
+  valz = map(az, -17000, 17000, 0, 179);
+  Serial.print("axis x = ") ;
+  Serial.print(valx) ;
+  Serial.print(" axis y = ") ;
+  Serial.print(valy) ;
+  Serial.print(" axis z = ") ;
+  Serial.println(valz) ;
+  if ( valy > 150) {
+    digitalWrite(ledR, HIGH);
+    digitalWrite(ledG, LOW);
+    digitalWrite(ledY, LOW);
+  }
+  if (valy  < 50) {
+    digitalWrite(ledR, LOW);
+    digitalWrite(ledG, HIGH);
+    digitalWrite(ledY, LOW);
+  }
+  if (valx < 50) {
+    digitalWrite(ledR, LOW);
+    digitalWrite(ledG, LOW);
+    digitalWrite(ledY, HIGH);
+  }
+  if (valz > 150) {
+    digitalWrite(ledR, LOW);
+    digitalWrite(ledG, LOW);
+    digitalWrite(ledY, LOW);
+  }
+  
+  delay(100);
+}*/
+
+
+
+#include "Wire.h"
+#include "I2Cdev.h"
+#include "MPU6050.h"
+MPU6050 mpu; //ประกาศตัวแปร mpu
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+void setup()
+{
+  Wire.begin();
+  Serial.begin(38400);
+
+  Serial.println("Initialize MPU");
+  mpu.initialize();
+  Serial.println(mpu.testConnection() ? "Connected" : "Connection failed");
+}
+
+void loop()
+{
+  mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  // แปลงค่าแกนให้เป็นมุม 0-180 องศา
+  ax = map(ax, -18000, 18000, 0, 180);
+  ay = map(ay, -18000, 18000, 0, 180);
+  az = map(az, -18000, 18000, 0, 180);
+  gx = map(gx, -18000, 18000, 0, 180);
+  gy = map(gy, -18000, 18000, 0, 180);
+  gz = map(gz, -18000, 18000, 0, 180);
+  Serial.print("Axyz \t");
+  Serial.print(ax); Serial.print("\t");
+  Serial.print(ay); Serial.print("AAAAA\t");
+  Serial.print(az); Serial.print("\t");
+  Serial.print("Gxyz \t");
+  Serial.print(gx); Serial.print("\t");
+  Serial.print(gy); Serial.print("\t");
+  Serial.println(gz);
+
+  delay(50);
+}
+
+
+
+/*#include "I2Cdev.h"
+#include "MPU6050.h"
+#include "Wire.h"
+MPU6050 mpu;
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+int valx , valy , valz;
+char rd;
+
+int prevVal;
+int led = 13 ;
+int pin11 = 6 , pin10 = 7 ;
+int val1 , val2 ;
+int valgy1 = 0 , valgy2 = 0;
+void setup() 
+
+{
+
+pinMode(led,OUTPUT) ;
+Wire.begin();
+Serial.begin(38400);
+Serial.println("Initialize MPU");
+mpu.initialize();
+Serial.println(mpu.testConnection() ? "Connected" : "Connection failed");
+
+}
+
+void loop() {
+mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+valx = map(ax, -17000, 17000, 0, 179);
+valy = map(ay, -17000, 17000, 0, 179);
+valz = map(az, -17000, 17000, 0, 179);
+
+Serial.print("axis x = ") ; 
+Serial.print(valx) ; 
+Serial.print(" axis y = ") ; 
+Serial.print(valy) ; 
+Serial.print(" axis z = ") ; 
+Serial.println(valz) ; 
+delay(100);
+
+}*/
+
+
+/*#include <Wire.h>
+#include "I2Cdev.h"
+#include "MPU6050.h"
+
+// MPU6050 Slave Device Address
+const uint8_t MPU6050SlaveAddress = 0x68;
+
+// Select SDA and SCL pins for I2C communication 
+const uint8_t scl = D6;
+const uint8_t sda = D7;
+
+// sensitivity scale factor respective to full scale setting provided in datasheet 
+const uint16_t AccelScaleFactor = 16384;
+const uint16_t GyroScaleFactor = 131;
+
+// MPU6050 few configuration register addresses
+const uint8_t MPU6050_REGISTER_SMPLRT_DIV   =  0x19;
+const uint8_t MPU6050_REGISTER_USER_CTRL    =  0x6A;
+const uint8_t MPU6050_REGISTER_PWR_MGMT_1   =  0x6B;
+const uint8_t MPU6050_REGISTER_PWR_MGMT_2   =  0x6C;
+const uint8_t MPU6050_REGISTER_CONFIG       =  0x1A;
+const uint8_t MPU6050_REGISTER_GYRO_CONFIG  =  0x1B;
+const uint8_t MPU6050_REGISTER_ACCEL_CONFIG =  0x1C;
+const uint8_t MPU6050_REGISTER_FIFO_EN      =  0x23;
+const uint8_t MPU6050_REGISTER_INT_ENABLE   =  0x38;
+const uint8_t MPU6050_REGISTER_ACCEL_XOUT_H =  0x3B;
+const uint8_t MPU6050_REGISTER_SIGNAL_PATH_RESET  = 0x68;
+
+int16_t AccelX, AccelY, AccelZ, Temperature, GyroX, GyroY, GyroZ;
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin(sda, scl);
+  MPU6050_Init();
+}
+
+void loop() {
+  double Ax, Ay, Az, T, Gx, Gy, Gz;
+  
+  Read_RawValue(MPU6050SlaveAddress, MPU6050_REGISTER_ACCEL_XOUT_H);
+  
+  //divide each with their sensitivity scale factor
+  Ax = (double)AccelX/AccelScaleFactor;
+  Ay = (double)AccelY/AccelScaleFactor;
+  Az = (double)AccelZ/AccelScaleFactor;
+  T = (double)Temperature/340+36.53; //temperature formula
+  Gx = (double)GyroX/GyroScaleFactor;
+  Gy = (double)GyroY/GyroScaleFactor;
+  Gz = (double)GyroZ/GyroScaleFactor;
+
+  Serial.print("Ax: "); Serial.print(Ax);
+  Serial.print(" Ay: "); Serial.print(Ay);
+  Serial.print(" Az: "); Serial.print(Az);
+  Serial.print(" T: "); Serial.print(T);
+  Serial.print(" Gx: "); Serial.print(Gx);
+  Serial.print(" Gy: "); Serial.print(Gy);
+  Serial.print(" Gz: "); Serial.println(Gz);
+
+  delay(100);
+}
+
+void I2C_Write(uint8_t deviceAddress, uint8_t regAddress, uint8_t data){
+  Wire.beginTransmission(deviceAddress);
+  Wire.write(regAddress);
+  Wire.write(data);
+  Wire.endTransmission();
+}
+
+// read all 14 register
+void Read_RawValue(uint8_t deviceAddress, uint8_t regAddress){
+  Wire.beginTransmission(deviceAddress);
+  Wire.write(regAddress);
+  Wire.endTransmission();
+  Wire.requestFrom(deviceAddress, (uint8_t)14);
+  AccelX = (((int16_t)Wire.read()<<8) | Wire.read());
+  AccelY = (((int16_t)Wire.read()<<8) | Wire.read());
+  AccelZ = (((int16_t)Wire.read()<<8) | Wire.read());
+  Temperature = (((int16_t)Wire.read()<<8) | Wire.read());
+  GyroX = (((int16_t)Wire.read()<<8) | Wire.read());
+  GyroY = (((int16_t)Wire.read()<<8) | Wire.read());
+  GyroZ = (((int16_t)Wire.read()<<8) | Wire.read());
+}
+
+//configure MPU6050
+void MPU6050_Init(){
+  delay(150);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_SMPLRT_DIV, 0x07);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_PWR_MGMT_1, 0x01);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_PWR_MGMT_2, 0x00);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_CONFIG, 0x00);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_GYRO_CONFIG, 0x00);//set +/-250 degree/second full scale
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_ACCEL_CONFIG, 0x00);// set +/- 2g full scale
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_FIFO_EN, 0x00);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_INT_ENABLE, 0x01);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_SIGNAL_PATH_RESET, 0x00);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_USER_CTRL, 0x00);
+}*/
+
+
+
+// (c) Michael Schoeffler 2017, http://www.mschoeffler.de
+
+/*#include "Wire.h" // This library allows you to communicate with I2C devices.
+
+const int MPU_ADDR = 0x68; // I2C address of the MPU-6050. If AD0 pin is set to HIGH, the I2C address will be 0x69.
+
+int16_t accelerometer_x, accelerometer_y, accelerometer_z; // variables for accelerometer raw data
+int16_t gyro_x, gyro_y, gyro_z; // variables for gyro raw data
+int16_t temperature; // variables for temperature data
+
+char tmp_str[7]; // temporary variable used in convert function
+
+char* convert_int16_to_str(int16_t i) { // converts int16 to string. Moreover, resulting strings will have the same length in the debug monitor.
+  sprintf(tmp_str, "%6d", i);
+  return tmp_str;
+}
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  Wire.beginTransmission(MPU_ADDR); // Begins a transmission to the I2C slave (GY-521 board)
+  Wire.write(0x6B); // PWR_MGMT_1 register
+  Wire.write(0); // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+}
+void loop() {
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H) [MPU-6000 and MPU-6050 Register Map and Descriptions Revision 4.2, p.40]
+  Wire.endTransmission(false); // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
+  Wire.requestFrom(MPU_ADDR, 7*2, true); // request a total of 7*2=14 registers
+  
+  // "Wire.read()<<8 | Wire.read();" means two registers are read and stored in the same variable
+  accelerometer_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
+  accelerometer_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
+  accelerometer_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x3F (ACCEL_ZOUT_H) and 0x40 (ACCEL_ZOUT_L)
+  temperature = Wire.read()<<8 | Wire.read(); // reading registers: 0x41 (TEMP_OUT_H) and 0x42 (TEMP_OUT_L)
+  gyro_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x43 (GYRO_XOUT_H) and 0x44 (GYRO_XOUT_L)
+  gyro_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x45 (GYRO_YOUT_H) and 0x46 (GYRO_YOUT_L)
+  gyro_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x47 (GYRO_ZOUT_H) and 0x48 (GYRO_ZOUT_L)
+  
+  // print out data
+  Serial.print("aX = "); Serial.print(convert_int16_to_str(accelerometer_x));
+  Serial.print(" | aY = "); Serial.print(convert_int16_to_str(accelerometer_y));
+  Serial.print(" | aZ = "); Serial.print(convert_int16_to_str(accelerometer_z));
+  // the following equation was taken from the documentation [MPU-6000/MPU-6050 Register Map and Description, p.30]
+  Serial.print(" | tmp = "); Serial.print(temperature/340.00+36.53);
+  Serial.print(" | gX = "); Serial.print(convert_int16_to_str(gyro_x));
+  Serial.print(" | gY = "); Serial.print(convert_int16_to_str(gyro_y));
+  Serial.print(" | gZ = "); Serial.print(convert_int16_to_str(gyro_z));
+  Serial.println();
+  
+  // delay
+  delay(1000);
+}*/
